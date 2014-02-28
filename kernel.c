348e3282adfa79c1dfbf61e0e1ee5f8e4de2f642
shinshipower <xu3fu6xup6@gmail.com>
2014-02-28 23:28:38 +0800

    Try make a test CMD

--- kernel.c
+++ kernel.c
@@ -106,7 +106,7 @@ void show_cmd_info(int argc, char *argv[]);
 void show_task_info(int argc, char *argv[]);
 void show_man_page(int argc, char *argv[]);
 void show_history(int argc, char *argv[]);
-
+void show_test(int argc, char *argv[]);
 /* Enumeration for command types. */
 enum {
 	CMD_ECHO = 0,
@@ -115,6 +115,7 @@ enum {
 	CMD_HISTORY,
 	CMD_MAN,
 	CMD_PS,
+	CMD_TEST,
 	CMD_COUNT
 } CMD_TYPE;
 /* Structure for command handler. */
@@ -129,7 +130,10 @@ const hcmd_entry cmd_data[CMD_COUNT] = {
 	[CMD_HELP] = {.cmd = "help", .func = show_cmd_info, .description = "List all commands you can use."},
 	[CMD_HISTORY] = {.cmd = "history", .func = show_history, .description = "Show latest commands entered."}, 
 	[CMD_MAN] = {.cmd = "man", .func = show_man_page, .description = "Manual pager."},
-	[CMD_PS] = {.cmd = "ps", .func = show_task_info, .description = "List all the processes."}
+	//[CMD_TEST] = {.cmd = "test", .func = show_test, . description = "This is for test"},
+	[CMD_PS] = {.cmd = "ps", .func = show_task_info, .description = "List all the processes."},
+	[CMD_TEST] = {.cmd = "test", .func = show_test, . description = "This is for test"}
+
 };
 
 /* Structure for environment variables. */
@@ -644,6 +648,13 @@ void export_envvar(int argc, char *argv[])
 		}
 	}
 }
+//test
+
+
+//void show_test(int argc, char* argv[])
+
+
+
 
 //ps
 void show_task_info(int argc, char* argv[])
@@ -676,6 +687,17 @@ void show_task_info(int argc, char* argv[])
 
 		write(fdout, &next_line , 3);
 	}
+}
+
+
+
+void show_test(int argc, char* argv[])
+{
+
+	write(fdout, "Test successfully",18);
+	write(fdout,next_line,3);
+
+
 }
 
 //this function helps to show int
